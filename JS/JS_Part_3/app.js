e7b5da2bdb9a10be315986535f02bf1a12a176ef
Aryan B V <aryansalian5678@gmail.com>
2024-10-09 23:09:09 +0530

    practised few codes

---
+++ JS/JS_Part_3/app.js
@@ -0,0 +1,11 @@
+// let msg = "    Hello     ";
+
+
+// let password = prompt("Enter the password");
+// console.log(password.trim());
+
+// let str = "I am Aryan";
+// console.log(str.indexOf("A"));  //finding the first index
+
+// let str = "hello";
+// console.log(str.slice(0,5));
